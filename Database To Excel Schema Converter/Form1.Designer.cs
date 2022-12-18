2412f650381bc0a73f462aed012780cb1cb5f40a
Thunis <thuniskruger@gmail.com>
2022-12-18 21:07:40 +0200

    Add project files.

---
+++ Database To Excel Schema Converter/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Database_To_Excel_Schema_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.xls = new DevExpress.XtraSpreadsheet.SpreadsheetControl();
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.spreadsheetCommandBarButtonItem1 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem2 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem3 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem4 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem5 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem6 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem7 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem8 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem9 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem10 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.spreadsheetCommandBarButtonItem11 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem();
+            this.fileRibbonPage1 = new DevExpress.XtraSpreadsheet.UI.FileRibbonPage();
+            this.commonRibbonPageGroup1 = new DevExpress.XtraSpreadsheet.UI.CommonRibbonPageGroup();
+            this.infoRibbonPageGroup1 = new DevExpress.XtraSpreadsheet.UI.InfoRibbonPageGroup();
+            this.spreadsheetBarController1 = new DevExpress.XtraSpreadsheet.UI.SpreadsheetBarController(this.components);
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.barButCreateXLS = new DevExpress.XtraBars.BarButtonItem();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spreadsheetBarController1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xls
+            // 
+            this.xls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xls.Location = new System.Drawing.Point(0, 158);
+            this.xls.MenuManager = this.ribbonControl1;
+            this.xls.Name = "xls";
+            this.xls.Size = new System.Drawing.Size(1034, 402);
+            this.xls.TabIndex = 0;
+            this.xls.Text = "spreadsheetControl1";
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.AutoHideEmptyItems = true;
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.ribbonControl1.SearchEditItem,
+            this.spreadsheetCommandBarButtonItem1,
+            this.spreadsheetCommandBarButtonItem2,
+            this.spreadsheetCommandBarButtonItem3,
+            this.spreadsheetCommandBarButtonItem4,
+            this.spreadsheetCommandBarButtonItem5,
+            this.spreadsheetCommandBarButtonItem6,
+            this.spreadsheetCommandBarButtonItem7,
+            this.spreadsheetCommandBarButtonItem8,
+            this.spreadsheetCommandBarButtonItem9,
+            this.spreadsheetCommandBarButtonItem10,
+            this.spreadsheetCommandBarButtonItem11,
+            this.barButCreateXLS});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 13;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.fileRibbonPage1});
+            this.ribbonControl1.Size = new System.Drawing.Size(1034, 158);
+            // 
+            // spreadsheetCommandBarButtonItem1
+            // 
+            this.spreadsheetCommandBarButtonItem1.CommandName = "FileNew";
+            this.spreadsheetCommandBarButtonItem1.Id = 1;
+            this.spreadsheetCommandBarButtonItem1.Name = "spreadsheetCommandBarButtonItem1";
+            // 
+            // spreadsheetCommandBarButtonItem2
+            // 
+            this.spreadsheetCommandBarButtonItem2.CommandName = "FileOpen";
+            this.spreadsheetCommandBarButtonItem2.Id = 2;
+            this.spreadsheetCommandBarButtonItem2.Name = "spreadsheetCommandBarButtonItem2";
+            // 
+            // spreadsheetCommandBarButtonItem3
+            // 
+            this.spreadsheetCommandBarButtonItem3.CommandName = "FileSave";
+            this.spreadsheetCommandBarButtonItem3.Id = 3;
+            this.spreadsheetCommandBarButtonItem3.Name = "spreadsheetCommandBarButtonItem3";
+            // 
+            // spreadsheetCommandBarButtonItem4
+            // 
+            this.spreadsheetCommandBarButtonItem4.CommandName = "FileSaveAs";
+            this.spreadsheetCommandBarButtonItem4.Id = 4;
+            this.spreadsheetCommandBarButtonItem4.Name = "spreadsheetCommandBarButtonItem4";
+            // 
+            // spreadsheetCommandBarButtonItem5
+            // 
+            this.spreadsheetCommandBarButtonItem5.CommandName = "FileQuickPrint";
+            this.spreadsheetCommandBarButtonItem5.Id = 5;
+            this.spreadsheetCommandBarButtonItem5.Name = "spreadsheetCommandBarButtonItem5";
+            // 
+            // spreadsheetCommandBarButtonItem6
+            // 
+            this.spreadsheetCommandBarButtonItem6.CommandName = "FilePrint";
+            this.spreadsheetCommandBarButtonItem6.Id = 6;
+            this.spreadsheetCommandBarButtonItem6.Name = "spreadsheetCommandBarButtonItem6";
+            // 
+            // spreadsheetCommandBarButtonItem7
+            // 
+            this.spreadsheetCommandBarButtonItem7.CommandName = "FilePrintPreview";
+            this.spreadsheetCommandBarButtonItem7.Id = 7;
+            this.spreadsheetCommandBarButtonItem7.Name = "spreadsheetCommandBarButtonItem7";
+            // 
+            // spreadsheetCommandBarButtonItem8
+            // 
+            this.spreadsheetCommandBarButtonItem8.CommandName = "FileUndo";
+            this.spreadsheetCommandBarButtonItem8.Id = 8;
+            this.spreadsheetCommandBarButtonItem8.Name = "spreadsheetCommandBarButtonItem8";
+            // 
+            // spreadsheetCommandBarButtonItem9
+            // 
+            this.spreadsheetCommandBarButtonItem9.CommandName = "FileRedo";
+            this.spreadsheetCommandBarButtonItem9.Id = 9;
+            this.spreadsheetCommandBarButtonItem9.Name = "spreadsheetCommandBarButtonItem9";
+            // 
+            // spreadsheetCommandBarButtonItem10
+            // 
+            this.spreadsheetCommandBarButtonItem10.CommandName = "FileEncrypt";
+            this.spreadsheetCommandBarButtonItem10.Id = 10;
+            this.spreadsheetCommandBarButtonItem10.Name = "spreadsheetCommandBarButtonItem10";
+            // 
+            // spreadsheetCommandBarButtonItem11
+            // 
+            this.spreadsheetCommandBarButtonItem11.CommandName = "FileShowDocumentProperties";
+            this.spreadsheetCommandBarButtonItem11.Id = 11;
+            this.spreadsheetCommandBarButtonItem11.Name = "spreadsheetCommandBarButtonItem11";
+            // 
+            // fileRibbonPage1
+            // 
+            this.fileRibbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.commonRibbonPageGroup1,
+            this.infoRibbonPageGroup1,
+            this.ribbonPageGroup1});
+            this.fileRibbonPage1.Name = "fileRibbonPage1";
+            // 
+            // commonRibbonPageGroup1
+            // 
+            this.commonRibbonPageGroup1.CaptionButtonVisible = DevExpress.Utils.DefaultBoolean.False;
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem1);
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem2);
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem3);
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem4);
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem5);
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem6);
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem7);
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem8);
+            this.commonRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem9);
+            this.commonRibbonPageGroup1.Name = "commonRibbonPageGroup1";
+            // 
+            // infoRibbonPageGroup1
+            // 
+            this.infoRibbonPageGroup1.CaptionButtonVisible = DevExpress.Utils.DefaultBoolean.False;
+            this.infoRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem10);
+            this.infoRibbonPageGroup1.ItemLinks.Add(this.spreadsheetCommandBarButtonItem11);
+            this.infoRibbonPageGroup1.Name = "infoRibbonPageGroup1";
+            // 
+            // spreadsheetBarController1
+            // 
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem1);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem2);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem3);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem4);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem5);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem6);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem7);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem8);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem9);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem10);
+            this.spreadsheetBarController1.BarItems.Add(this.spreadsheetCommandBarButtonItem11);
+            this.spreadsheetBarController1.Control = this.xls;
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButCreateXLS);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+            // 
+            // barButCreateXLS
+            // 
+            this.barButCreateXLS.Caption = "Create XLS";
+            this.barButCreateXLS.Id = 12;
+            this.barButCreateXLS.Name = "barButCreateXLS";
+            this.barButCreateXLS.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButCreateXLS_ItemClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 560);
+            this.Controls.Add(this.xls);
+            this.Controls.Add(this.ribbonControl1);
+            this.Name = "Form1";
+            this.Ribbon = this.ribbonControl1;
+            this.Text = "DB Schema to XLS";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spreadsheetBarController1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraSpreadsheet.SpreadsheetControl xls;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem1;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem2;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem3;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem4;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem5;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem6;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem7;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem8;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem9;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem10;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetCommandBarButtonItem spreadsheetCommandBarButtonItem11;
+        private DevExpress.XtraSpreadsheet.UI.FileRibbonPage fileRibbonPage1;
+        private DevExpress.XtraSpreadsheet.UI.CommonRibbonPageGroup commonRibbonPageGroup1;
+        private DevExpress.XtraSpreadsheet.UI.InfoRibbonPageGroup infoRibbonPageGroup1;
+        private DevExpress.XtraSpreadsheet.UI.SpreadsheetBarController spreadsheetBarController1;
+        private DevExpress.XtraBars.BarButtonItem barButCreateXLS;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+    }
+}
+
---
+++ Database To Excel Schema Converter/Form1.cs
@@ -0,0 +1,83 @@
+﻿using DevExpress.ClipboardSource.SpreadsheetML;
+using DevExpress.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database_To_Excel_Schema_Converter
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        private IWorkbook workbook;
+
+        public string ConString { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            ConString = "Data Source=THUNIS-FLIGHT;Initial Catalog=DataBRI;Integrated Security=True;Asynchronous Processing=true;";
+        }
+
+        private void CreateXLSSheetAndPopulateColumnData(string TableName)
+        {
+            String[] tableRestrictions = new String[4];
+
+            tableRestrictions[2] = TableName;
+            using (SqlConnection conn = new SqlConnection(ConString))
+            {
+                conn.Open();
+                DataTable metaDataTable = conn.GetSchema("Columns", tableRestrictions);
+                metaDataTable.DefaultView.Sort="ORDINAL_POSITION";
+
+                DataTable dtSort = metaDataTable.DefaultView.ToTable();
+
+                workbook = xls.Document;
+                workbook.Worksheets.Add().Name = metaDataTable.Rows[0]["TABLE_NAME"].ToString();
+                DevExpress.Spreadsheet.Worksheet ws = workbook.Worksheets[metaDataTable.Rows[0]["TABLE_NAME"].ToString()];
+                ws.Cells[0, 0].Value = "Column Name";
+                ws.Cells[0,0].ColumnWidth= 800;
+                ws.Cells[0, 1].Value = "Data Type";
+                ws.Cells[0, 1].ColumnWidth = 400;
+
+                for (int i = 0; i < dtSort.Rows.Count; i++)
+                {
+                    ws.Cells[i+1, 0].Value = dtSort.Rows[i]["COLUMN_NAME"].ToString();
+                    ws.Cells[i + 1, 1].Value = dtSort.Rows[i]["DATA_TYPE"].ToString();
+
+                }
+
+
+            }
+        }
+
+        private DataTable GetSchema()
+        {
+            using (SqlConnection conn = new SqlConnection(ConString))
+            {
+                conn.Open();
+                DataTable metaDataTable = conn.GetSchema("Tables");
+                return metaDataTable;
+            }
+
+        }
+
+        private void barButCreateXLS_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            DataTable dt = GetSchema();
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (dr["TABLE_TYPE"].ToString() == "BASE TABLE")
+                {
+                    CreateXLSSheetAndPopulateColumnData(dr["TABLE_NAME"].ToString());
+                }
+            }
+
+        }
+    }
+}
